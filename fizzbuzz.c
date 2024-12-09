8fc8319cfb228d759375868aa11e71040e99facf
Gabriel <gabrielpezoa26@gmail.com>
2024-12-09 11:29:11 -0300

    katzenn

--- fizzbuzz.c
+++ fizzbuzz.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   fizzbuzz.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gcesar-n <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: gabriel <gabriel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/05 14:06:06 by gcesar-n          #+#    #+#             */
-/*   Updated: 2024/12/05 14:25:27 by gcesar-n         ###   ########.fr       */
+/*   Updated: 2024/12/07 14:02:37 by gabriel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,30 +19,25 @@ void	fizzbuzz(void)
 	int	loko_mango;
 
 	number = 1;
-	mango_loko = number % 3;
-	loko_mango = number % 5;
 	while (number <= 100)
 	{
+	mango_loko = number % 3;
+	loko_mango = number % 5;
+		if (mango_loko == 0 && loko_mango == 0)
+		{
+			write(1, "fizzbuzz\n", 9);
+		}
 		if (mango_loko == 0)
 		{
-			write(1, "fizz", 4);
-			number++;
+			write(1, "fizz\n", 5);
 		}
 		if (loko_mango == 0)
 		{
-			write(1, "buzz", 4);
-			number++;
-		}
-		if (mango_loko == 0 && loko_mango == 0)
-		{
-			write(1, "fizzbuzz", 1);
-			number++;
+			write(1, "buzz\n", 5);
 		}
 		write(1, &number, 1);
-		number++;		
+		number++;
 	}
-
-
 }
 
 int	main(void)
