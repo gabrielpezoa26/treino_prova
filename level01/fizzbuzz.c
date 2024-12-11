d92aab1f3ee759516470567c82e27ddd8d767e1d
Gabriel <gabrielpezoa26@gmail.com>
2024-12-11 13:10:05 -0300

    fsdfdsdf

---
+++ level01/fizzbuzz.c
@@ -0,0 +1,51 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   fizzbuzz.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gabriel <gabriel@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/05 14:06:06 by gcesar-n          #+#    #+#             */
+/*   Updated: 2024/12/11 12:34:26 by gabriel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	print_number(int katana)
+{
+	char	digit;
+
+	if (katana >= 10)
+		print_number(katana / 10);
+	digit = (katana % 10) + '0';
+	write(1, &digit, 1);
+}
+
+void	fizzbuzz(void)
+{
+	int number;
+
+	number = 1;
+	while (number <= 100)
+	{
+		if (number % 3 == 0 && number % 5 == 0)
+			write(1, "fizzbuzz\n", 9);
+		else if (number % 3 == 0)
+			write(1, "fizz\n", 5);
+		else if (number % 5 == 0)
+			write(1, "buzz\n", 5);
+		else
+		{
+			print_number(number);
+			write(1, "\n", 1);
+		}
+		number++;
+	}
+}
+
+int	main(void)
+{
+	fizzbuzz();
+	return (0);
+}
---
+++ level01/ulstr.c
@@ -0,0 +1,73 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ulstr.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gabriel <gabriel@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/04 10:19:18 by gcesar-n          #+#    #+#             */
+/*   Updated: 2024/12/11 12:32:17 by gabriel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+
+char	*mango_loko(char *string)
+{
+	int	index;
+
+	index = 0;
+	while (string[index] != '\0')
+	{
+		if (string[index] >= 'a' && string[index] <= 'z')
+			string[index] -= 32;
+		else if (string[index] >= 'A' && string [index] <= 'Z')
+			string[index] += 32;
+		index++;
+	}
+	return (string);
+}
+
+int	main(int argc, char **argv)
+{
+	if (argc != 2)
+	{
+		write(1, "\n", 1);
+		return (0);
+	}
+
+	char *result = mango_loko(argv[1]);
+	int len = 0;
+	while (result[len] != '\0')
+		len++;
+
+	write(1, result, len);
+	write(1, "\n", 1);
+	return (0);
+}
+
+/*
+	Assignment name  : ulstr
+Expected files   : ulstr.c
+Allowed functions: write
+--------------------------------------------------------------------------------
+
+Write a program that takes a string and reverses the case of all its letters.
+Other characters remain unchanged.
+
+You must display the result followed by a '\n'.
+
+If the number of arguments is not 1, the program displays '\n'.
+
+Examples :
+
+$>./ulstr "L'eSPrit nE peUt plUs pRogResSer s'Il staGne et sI peRsIsTent VAnIte et auto-justification." | cat -e
+l'EspRIT Ne PEuT PLuS PrOGrESsER S'iL STAgNE ET Si PErSiStENT vaNiTE ET AUTO-JUSTIFICATION.$
+$>./ulstr "S'enTOuRer dE sECreT eSt uN sIGnE De mAnQuE De coNNaiSSanCe.  " | cat -e
+s'ENtoUrER De SecREt EsT Un SigNe dE MaNqUe dE COnnAIssANcE.  $
+$>./ulstr "3:21 Ba  tOut  moUn ki Ka di KE m'en Ka fe fot" | cat -e
+3:21 bA  ToUT  MOuN KI kA DI ke M'EN kA FE FOT$
+$>./ulstr | cat -e
+$
+*/
