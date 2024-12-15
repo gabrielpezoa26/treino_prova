302431a57917099424d604b9bed11149d32d782f
Gabriel <gabrielpezoa26@gmail.com>
2024-12-15 16:30:41 -0300

    gsfdsfs

---
+++ level02/ft_atoi.c
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_atoi.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gabriel <gabriel@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/15 16:22:39 by gabriel           #+#    #+#             */
+/*   Updated: 2024/12/15 16:29:08 by gabriel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+int	ft_atoi(const char *str)
+{
+	int	i;
+	int	mango_loko;
+	int	signal;
+
+	i = 0;
+	mango_loko = 0;
+	signal = 1;
+	while ((str[i] >= 9 && str[i] <= 13) || str[i] == 32)
+	{
+		i++;
+	}
+	if (str[i] == '-')
+	{
+		signal = (-1);
+		i++;
+	}
+	else if (str[i] == '+')
+		i++;
+	while (str[i] >= '0' && str[i] <= '9')
+	{
+		mango_loko = mango_loko * 10 + (str[i] - '0');
+		i++;
+	}
+	return (mango_loko * signal);
+}
+
+int main(void)
+{
+	char *potato = "  -863";
+
+	printf("%d\n", ft_atoi(potato));
+}
+/*
+Assignment name  : ft_atoi
+Expected files   : ft_atoi.c
+Allowed functions: None
+--------------------------------------------------------------------------------
+
+Write a function that converts the string argument str to an integer (type int)
+and returns it.
+
+It works much like the standard atoi(const char *str) function, see the man.
+
+Your function must be declared as follows:
+
+int	ft_atoi(const char *str);
+*/
