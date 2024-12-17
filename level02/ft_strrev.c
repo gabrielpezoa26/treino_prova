b8ee7bb377dfa368219c2536158c86b12cc32c99
Gabriel <gabrielpezoa26@gmail.com>
2024-12-17 20:25:03 -0300

    strrev

---
+++ level02/ft_strrev.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strrev.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gabriel <gabriel@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/17 20:12:28 by gabriel           #+#    #+#             */
+/*   Updated: 2024/12/17 20:24:49 by gabriel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <stdio.h>
+
+char	*ft_strrev(char *str)
+{
+	int len;
+	int start;
+	int end;
+	char temp;
+
+	len = 0;
+	while (str[len] != '\0')
+	{
+		len++;
+	}
+	end = len - 1;
+	start = 0;
+	while (start < end)
+	{
+		temp = str[start];
+		str[start] = str[end];
+		str[end] = temp;
+		start++;
+		end--;
+	}
+	return (str);
+}
+
+int	main()
+{
+	char kopf[] = "abcdefghijkl";
+
+	printf("%s", ft_strrev(kopf));
+}
+/*
+Assignment name  : ft_strrev
+Expected files   : ft_strrev.c
+Allowed functions:
+--------------------------------------------------------------------------------
+
+Write a function that reverses (in-place) a string.
+
+It must return its parameter.
+
+Your function must be declared as follows:
+
+char    *ft_strrev(char *str);
+*/
