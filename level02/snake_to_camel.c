013ba28433f717869db47974b7968b8a398f39cf
Gabriel <gabrielpezoa26@gmail.com>
2025-01-08 14:02:36 -0300

    fsfdsfds

---
+++ level02/snake_to_camel.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   snake_to_camel.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gabriel <gabriel@student.42.fr>            +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/08 13:34:01 by gabriel           #+#    #+#             */
+/*   Updated: 2025/01/08 14:02:11 by gabriel          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+int main(int argc, char **argv)
+{
+	int i;
+
+	i = 0;
+	if (argc == 2)
+	{
+		while(argv[1][i] != '\0')  //percorre a string ate o final
+		{
+			if (argv[1][i] == '_')
+			{
+				i++;
+				argv[1][i] = argv[1][i] - 32;
+			}
+			write (1, &argv[1][i], 1);
+			i++;
+		}
+	}
+	write(1, "\n", 1);  //poe a nova linha independente do 'argc'
+	return (0);
+}
+
+/*Assignment name  : snake_to_camel
+Expected files   : snake_to_camel.c
+Allowed functions: malloc, free, realloc, write
+--------------------------------------------------------------------------------
+
+Write a program that takes a single string in snake_case format
+and converts it into a string in lowerCamelCase format.
+
+A snake_case string is a string where each word is in lower case, separated by
+an underscore "_".
+
+A lowerCamelCase string is a string where each word begins with a capital letter
+except for the first one.
+
+Examples:
+$>./camel_to_snake "here_is_a_snake_case_word"
+hereIsASnakeCaseWord
+$>./camel_to_snake "hello_world" | cat -e
+helloWorld$
+$>./camel_to_snake | cat -e\
+$*/
