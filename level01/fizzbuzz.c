8fbbd8076eea9ca6c7a4661b36a6c7aac2914b05
Gabriel Cesar Nunes Pezoa <gcesar-n@c1r5p4.42sp.org.br>
2025-01-18 13:31:13 -0300

    dasda

--- level01/fizzbuzz.c
+++ level01/fizzbuzz.c
@@ -6,23 +6,13 @@
 /*   By: gcesar-n <gcesar-n@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/05 14:06:06 by gcesar-n          #+#    #+#             */
-/*   Updated: 2025/01/18 08:51:07 by gcesar-n         ###   ########.fr       */
+/*   Updated: 2025/01/18 13:27:04 by gcesar-n         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
 
 void	ft_putnbr(int n)
-{
-	char number;
-
-	if (n >= 10)
-		ft_putnbr(n / 10);
-	number = (n % 10) + '0';
-	write(1, &number, 1);
-}
-
-void ft_putnbr(int n)
 {
 	char number;
 
@@ -40,21 +30,13 @@ int		main(void)
 	while (i <= 100)
 	{
 		if (i % 15 == 0)
-		{
 			write (1, "fizzbuzz", 8);
-		}
 		else if (i % 3 == 0)
-		{
 			write (1, "fizz", 4);
-		}
 		else if (i % 5 == 0)
-		{
 			write (1, "buzz", 4);
-		}
 		else
-		{
 			ft_putnbr(i);
-		}
 		i++;
 		write (1, "\n", 1);
 	}
