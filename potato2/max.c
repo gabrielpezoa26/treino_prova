6c6ba2f4fbebb4b19b6adf75106e0237042c079f
Gabriel <gabrielpezoa26@gmail.com>
2025-01-19 16:45:24 -0300

    dadsA

--- potato2/max.c
+++ potato2/max.c
@@ -6,24 +6,37 @@
 /*   By: gabriel <gabriel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/19 16:26:02 by gabriel           #+#    #+#             */
-/*   Updated: 2025/01/19 16:29:41 by gabriel          ###   ########.fr       */
+/*   Updated: 2025/01/19 16:36:54 by gabriel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 
-int max(int *tab, unsigned int len)
+static int ft_max(int *tab, unsigned int len)
 {
 	int i;
 	int result;
 
 	if (!*tab || len == 0)
 		return (0);
-	result = tab[i];
+	result = tab[0];
+	i = 1;
 	while (i < len)
 	{
-		if ()
+		if (tab[i] > result)
+			result = tab[i];
+		i++;
 	}
+	return (result);
+}
+
+int main(void)
+{
+	int array[] = {1, 3, 7, 2, 5};
+	int empty[10];
+	
+	printf("%d\n", ft_max(array, 5));
+	//printf("%d\n", ft_max(empty, 5));
 }
 /*
 Assignment name  : max
