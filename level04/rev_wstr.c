46228d8ee72e2e39130a7869d2f9174e115d2ae4
Gabriel <gabrielpezoa26@gmail.com>
2025-01-18 23:24:50 -0300

    ag

--- level04/rev_wstr.c
+++ level04/rev_wstr.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   rev_wstr.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gcesar-n <gcesar-n@student.42.fr>          +#+  +:+       +#+        */
+/*   By: gabriel <gabriel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/12 15:01:12 by gabriel           #+#    #+#             */
-/*   Updated: 2025/01/16 17:25:17 by gcesar-n         ###   ########.fr       */
+/*   Updated: 2025/01/18 18:33:01 by gabriel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,14 +28,10 @@ int main(int argc, char **argv)
 		while(i >= 0)  //começa a iterar, de tras p frente
 		{
 			while( argv[1][i] == '\0' || argv[1][i] == ' ' || argv[1][i] == '\t')  //tira o espaço em branco depois da string
-			{
 				i--;  //tira o espaço em branco depois da string
-			}
-			end = i;  //marca o ultimo char 
+			end = i; //marca o ultimo char 
 			while(argv[1][i] && argv[1][i] != ' ' && argv[1][i] != '\t')  //percorre a palavra ate o primeiro char
-			{
 				i--;
-			}
 			start = i + 1;  //marca o primeiro char da palavra atual
 			flag = start;
 			while(start <= end)  //printa os bagui
