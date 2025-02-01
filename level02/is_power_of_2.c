17e8a849101c94fb0a1807174bcf03571f7246a5
gabriel <gabrielpezoa26@gmail.com>
2025-01-31 22:18:40 -0300

    -

--- level02/is_power_of_2.c
+++ level02/is_power_of_2.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   is_power_of_2.c                                    :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gcesar-n <gcesar-n@student.42.fr>          +#+  +:+       +#+        */
+/*   By: gabriel <gabriel@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/17 11:11:19 by gcesar-n          #+#    #+#             */
-/*   Updated: 2025/01/26 11:29:04 by gcesar-n         ###   ########.fr       */
+/*   Updated: 2025/01/31 22:17:14 by gabriel          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,16 +21,6 @@ int	is_power_of_2(unsigned int n)
 		n /= 2;
 	return (n == 1);
 }
-
-/*
-	if(n == 0)
-		return (0);
-	if(n == 1)
-		return (1);
-	while (n >= 2)
-		n = n /2;
-	return (0);
-*/
 
 //teste
 int	main(void)
